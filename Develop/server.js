5aab72a0ac2f43178d3ac3201cc848eb1ded4f52
Charlee <Charleslee1904@outlook.com>
2021-04-19 22:33:51 -0700

    fixing server.js

---
+++ Develop/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const db= require("./db/db.json");
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+
+app.use(express.urlencoded({ extended:false}));
+app.use(express.json());
+
+//makes the landing page static
+app.use(express.static(path.join(__dirname,'/public')));
+
+//gets the notes page
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'public','notes.html')));
+
+//gets saved notes
+app.get('/api/notes',(req,res)=> {
+  const saveFs = JSON.parse(fs.readFileSync('db/db.json',"utf8"));
+  res.json(saveFs);
+});
+
+
+app.post('/api/notes',(req,res)=>{
+   let newNote = req.body
+   newNote.id = uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+   const saveFs = JSON.parse(fs.readFileSync('db/db.json',"utf8"));
+    saveFs.push(newNote);
+    console.log(saveFs)
+    fs.writeFileSync('db/db.json',JSON.stringify(saveFs));
+    res.json(newNote)
+});
+
+
+app.delete('api/notes/:id',(req,res)=>{
+const id = req.params.id;
+id.delete(id);
+fs.writeFileSync('db/db.json',JSON.stringify(db));
+});
+
+
+app.listen(PORT, () => console.log('Server started on PORT 3000'));
